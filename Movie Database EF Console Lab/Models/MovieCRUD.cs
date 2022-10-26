bbcf973f3938aaf38b74e80a3859cc58fa01fcf2
Jessie Whiting <jessienwhiting@gmail.com>
2022-10-25 21:18:58 -0400

    Add project files.

---
+++ Movie Database EF Console Lab/Models/MovieCRUD.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Movie_Database_EF_Console_Lab;
+using static System.Collections.Specialized.BitVector32;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Movie_Database_EF_Console_Lab.Models
+{
+    internal class MovieCRUD
+    {
+        public void userSelection()
+        {
+            bool goAgain = true;
+
+            MovieContext db = new MovieContext();
+
+            List<Movie> movies = db.Movies.ToList();
+
+
+            Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+            Console.WriteLine("Movie List: ");
+            Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+
+            while (goAgain)
+            {
+                Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                Console.WriteLine("Enter |Title[T]| OR |GENRE[G]|: ");
+                Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+               
+                    string user = Console.ReadLine().ToUpper();
+                    Console.Clear();
+                if (user == "GENRE" || user == "G")
+                {
+
+                    Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    Console.WriteLine("Enter Genre: ");
+                    Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    Console.WriteLine("Horror");
+                    Console.WriteLine("Action");
+                    Console.WriteLine("Drama");
+                    Console.WriteLine("Comedy");
+                    Console.WriteLine("Romance");
+
+                    string userInput = Console.ReadLine().ToUpper();
+
+                    if (user == "HORROR" || user == "ACTION" || user == "DRAMA" || user =="COMEDY"
+                        || user == "ROMANCE")
+                    {
+                    List<Movie> listGenres = SearchByGenre(userInput, movies);
+
+                    PrintMovies(listGenres);
+
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                        Console.WriteLine("GENRE UNAVAILABLE");
+                        Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                        Console.ResetColor();
+                        continue;
+                    }
+                  
+
+                }
+                else if (user == "TITLE" || user == "T")
+                {
+                    Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    Console.WriteLine("Enter Title: ");
+                    Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+                    Console.WriteLine("The Conjuring ");
+                    Console.WriteLine("Pulp Fiction ");
+                    Console.WriteLine("The Shawshank Redemption ");
+                    Console.WriteLine("Trick 'r Treat ");
+                    Console.WriteLine("Idiocracy ");
+                    Console.WriteLine("Gladiator ");
+                    Console.WriteLine("Meet The Parents ");
+                    Console.WriteLine("Jurassic Park ");
+
+                    string input = Console.ReadLine().ToUpper().Trim();
+
+                    List<Movie> listTitles = SearchByTitle(input, movies);
+                    if (input == "THE CONJURING" || input == "PULP FICTION" || input == "THE SHAWSHANK REDEMPTION"
+                        || input == "TRICK 'R TREAT" || input == "IDIOCRACY" || input == "GLADIATOR" || input == "MEET THE PARENTS"
+                        || input == "JURASSIC PARK")
+                    {
+                    PrintMovies(listTitles);
+
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    Console.WriteLine("TITLE UNAVAILABLE");
+                    Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    Console.ResetColor();
+                    continue;
+                }
+
+                        goAgain = GoAgain();
+            }
+        }
+        public bool GoAgain()
+        {
+            Console.WriteLine("Search Again |YES[Y]| OR |NO[N]|?: ");
+            string input = Console.ReadLine().ToUpper();
+
+            if (input == "YES" || input == "Y")
+            {
+                return true;
+            }
+            else if (input == "NO" || input == "N")
+            {
+                Console.WriteLine("EXIT: ");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("INVALID INPUT");
+                return GoAgain();
+            }
+        }
+
+        //Method for TITLE LIST
+        public List<Movie> SearchByTitle(string userInput, List<Movie> movies)
+        {
+
+            List<Movie> listTitles = movies.Where(n => n.Title.ToUpper() == userInput).ToList();
+    
+
+            return listTitles;
+        }
+        //Method for GENRE LIST
+        public List<Movie> SearchByGenre(string userInput, List<Movie> movies)
+        {
+
+            List<Movie> listGenres = movies.Where(n => n.Genre.ToUpper() == userInput).ToList();
+
+            return listGenres;
+        }
+        public void PrintMovies(List<Movie> movieList)
+        {
+            Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+            Console.WriteLine("MOVIE DETAILS: ");
+            Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+            foreach (Movie movie in movieList)
+            {
+             
+                Console.WriteLine($"{movie.Id}.) MOVIE: {movie.Title}||| GENRE: {movie.Genre}|||RUNTIME: {movie.Runtime} MINS");
+             
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Movie Database EF Console Lab/Program.cs
@@ -0,0 +1,31 @@
+﻿using Movie_Database_EF_Console_Lab.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Intrinsics.X86;
+using Movie_Database_EF_Console_Lab;
+using System.Security.Cryptography.X509Certificates;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Movie_Database_EF_Console_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MovieCRUD sc = new MovieCRUD();
+
+            sc.userSelection();
+
+        }
+
+
+    }
+}
+          
+       
+
+
+
+
+
+
